0d09640fd2ab1ca5e1aa6777eeee34c0a3494800
Spimy <officialspyme@gmail.com>
2023-09-23 20:59:44 +0800

    👌 IMPROVE: Wrap slot machine in modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import Modal from '$lib/components/modal.svelte';
 	import SlotMachine from '$lib/components/slot-machine.svelte';
 	import '../scss/main.scss';
 </script>
 
-<SlotMachine />
+<Modal bgText={'You must win the slot machine in order to enter the portfolio.'}>
+	<SlotMachine />
+</Modal>
 
 <style>
 </style>
