b3c22b9a56e152c48e8fcaf501d49c6f5a20992b
Spimy <officialspyme@gmail.com>
2023-09-23 21:51:12 +0800

    👌 IMPROVE: Load portfolio content if cookie is set to passed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,19 @@
 	import Modal from '$lib/components/modal.svelte';
 	import SlotMachine from '$lib/components/slot-machine.svelte';
 	import '../scss/main.scss';
+	import type { LayoutServerData } from './$types';
+
+	export let data: LayoutServerData;
+	console.log(data);
 </script>
 
-<Modal bgText={'You must win the slot machine in order to enter the portfolio.'}>
-	<SlotMachine />
-</Modal>
+{#if data.passed === 'true'}
+	<slot />
+{:else}
+	<Modal bgText={`You must win the slot machine in order to enter the portfolio. ${data.passed}`}>
+		<SlotMachine />
+	</Modal>
+{/if}
 
 <style>
 </style>
