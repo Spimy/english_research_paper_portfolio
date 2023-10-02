4674e3dee2c398f82ebcbb3e864159e3f303c7dd
Spimy <officialspyme@gmail.com>
2023-10-02 10:38:37 +0800

    👌 IMPROVE: Temporarily disable slot machine

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import Modal from '$lib/components/modal.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
-	import SlotMachine from '$lib/components/slot-machine.svelte';
 	import '../scss/main.scss';
 	import type { LayoutServerData } from './$types';
 
@@ -9,11 +7,11 @@
 </script>
 
 <!-- TODO: Slow down the switch from modal to content on pass -->
-{#if data.passed !== 'true'}
+<!-- {#if data.passed !== 'true'}
 	<Modal bgText={`You must win the slot machine in order to enter the portfolio. ${data.passed}`}>
 		<SlotMachine />
 	</Modal>
-{/if}
+{/if} -->
 
 <Navbar />
 <slot />
