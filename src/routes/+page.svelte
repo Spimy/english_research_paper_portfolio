74854f2ac3c8646f824cd31f7a41a1a50ab327ea
Spimy <officialspyme@gmail.com>
2023-10-02 14:52:41 +0800

    📦 NEW: Add hero section to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Homepage Content!</h1>
+<script lang="ts">
+</script>
+
+<main>
+	<div class="hero">
+		<h1>Online Gambling</h1>
+	</div>
+</main>
+
+<style lang="scss">
+	.hero {
+		background-image: url('/hero.jpg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-attachment: fixed;
+		background-color: hsla(0, 0%, 0%, 0.5);
+		background-blend-mode: multiply;
+		height: 100svh;
+		display: grid;
+		place-items: center;
+	}
+</style>
