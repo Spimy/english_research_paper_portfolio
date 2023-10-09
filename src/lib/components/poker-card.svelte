6e8e90f2124667d01d13b9dfa348efd5e78581a4
Spimy <officialspyme@gmail.com>
2023-10-09 16:51:07 +0800

    📦 NEW: Add poker card component

---
+++ src/lib/components/poker-card.svelte
@@ -0,0 +1,92 @@
+<script lang="ts" context="module">
+	export enum CardSuit {
+		CLUBS = '♣',
+		DIAMONDS = '♦',
+		HEARTS = '♥',
+		SPADES = '♠'
+	}
+
+	export interface PokerCard {
+		suit: CardSuit;
+		display: 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K';
+	}
+</script>
+
+<script lang="ts">
+	export let cardSetting: PokerCard;
+</script>
+
+<div class="card">
+	<div class="card__content">
+		<div class="card__content--front">
+			<slot />
+		</div>
+		<div class="card__content--back" />
+	</div>
+</div>
+
+<style lang="scss">
+	.card {
+		position: relative;
+		width: 20rem;
+		height: 30rem;
+		perspective: 500px;
+		cursor: pointer;
+
+		&__content {
+			position: absolute;
+			width: 100%;
+			height: 100%;
+			box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+			transition: transform 1s;
+			transform-style: preserve-3d;
+
+			&--front,
+			&--back {
+				position: absolute;
+				display: grid;
+				place-items: center;
+				height: 100%;
+				width: 100%;
+				background: var(--clr-neutral-100);
+				color: var(--clr-background-200);
+				text-align: center;
+				font-size: 60px;
+				border-radius: 5px;
+				backface-visibility: hidden;
+			}
+
+			&--front {
+				background: white;
+				background-image: repeating-linear-gradient(
+						to right,
+						transparent 0,
+						transparent 9px,
+						rgba(255, 255, 255, 0.2) 9px,
+						rgba(208, 208, 208, 0.2) 10px
+					),
+					linear-gradient(to bottom left, rgba(233, 233, 233, 0.483), rgba(255, 255, 255, 0.05));
+				background-size: 0.65em 0.65em;
+			}
+
+			&--back {
+				background: linear-gradient(to left, var(--clr-neutral-100) 50%, var(--clr-cta-100) 50%);
+				background-size: 5%;
+				color: var(--clr-neutral-100);
+				transform: rotateY(180deg);
+			}
+		}
+
+		transform: scale(90%);
+		transition: transform 750ms ease-in-out;
+
+		&:hover {
+			transform: scale(100%);
+		}
+
+		&:hover &__content {
+			transform: rotateY(360deg);
+			transition: transform 750ms ease-in-out;
+		}
+	}
+</style>
