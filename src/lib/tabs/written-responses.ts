5956160e6d65dcee34ee0622a1180ac96a213c39
Spimy <officialspyme@gmail.com>
2023-10-30 18:03:10 +0800

    📦 NEW: Add written response data

---
+++ src/lib/tabs/written-responses.ts
@@ -0,0 +1,72 @@
+import type { Tab } from '$lib/types/tab';
+
+export const writtenResponses: Tab[] = [
+	{
+		query: 'william',
+		authors: [
+			{
+				firstName: 'William',
+				lastName: 'Law Hong Waye',
+				studentId: '23020043'
+			}
+		],
+		title: 'A written response on mental health and underaged gambling',
+		content:
+			'Concerns regarding the influence of online gambling affecting the mental health of its users, especially ones on the younger side, has been on the rise. Legal measures must be taken as online gambling can affect mental health from a young age. This is highlighted by Livazović and Bojčić (2019) which indicated that 5.8% of adolescents have admitted to online betting, revealing that there is indeed a significant prevalence of gambling behaviour among this demographic. This enables addiction to develop at an early age, especially since online gambling is easily accessible. The most common way for a teenager, or even a child, to be exposed to online gambling is through video games with a gacha or loot box system. A study by Lakić et al. (2023) investigated the addictive nature of gacha games and their players’ spending behaviours where they concluded they are a form of online gambling due to shared, similar, mechanics. From this conclusion, we could also infer that this applies to games with a loot box system. Addiction developed from these types of games can lead to increased anxiety and stress level, and eventually depression. Moreover, a cognitive bias called the sunk cost fallacy, a bias that tampers with decision-making, gets developed. It is a persistence that keeps individuals wanting to invest resources, be it time or money, even when confronted with unfavourable odds or prior losses. Tait et al. (2019) has also highlighted that younger adults are being primarily affected, especially when money is involved. They reached this conclusion by sorting age groups and their investment types. Based on these readings, I believe that more efforts are required to address these concerns and develop responsible gambling practices as beyond psychological consequences, they also raise ethical and legal questions. The exploitability of these behaviours, particularly among younger individuals, demands for stricter legal frameworks that are to be obeyed so as the vulnerable young ones are protected.',
+		references: [],
+		download: {
+			url: '/written-responses/G1_William_23020043_Written-Response_vFinal.pdf',
+			filename: 'G1_William_23020043_Written-Response_vFinal.pdf'
+		},
+		grade: {
+			received: 40,
+			maximum: 40
+		}
+	},
+	{
+		query: 'joshua',
+		authors: [
+			{
+				firstName: 'Joshua',
+				lastName: 'Edwin Rene Bonham',
+				studentId: '23020050'
+			}
+		],
+		title:
+			'Assessing the unregulated deceptive marketing of online gambling and its effects on youth: A brief response on significant articles regarding its drawbacks',
+		content:
+			'With the mass advancement in IT and internet capabilities, online gambling (OG) has become increasingly available, contributing to the augmentation of advertising and marketing of OG. However, many of these marketing schemes deliberately tap into young consumer subjectivity, creating a co-occurrence with problem gambling and financial losses. This brief response will solidify that the moderately unregulated deceptive marketing of OG has ramifications amongst youth. Firstly, López-González et al. (2017) emphasise how strategies such as skill-enhancing and risk-lowering advertising, heuristic of representativeness, intrinsic sensory, celebrity endorsements, and free-money incentives contribute to ludomania. According to research they gathered, supporters of the Australian Rugby League, many of whom are families with pubescent children, were subject to 322 episodes of gambling advertisements throughout three matches (López-González et al., 2017). They argue, as a result, that minors may be susceptible to early-onset gambling addiction. Furthermore, Abarbanel et al. (2020) explore the use of Twitch for mechanisms in gambling engagement amongst viewers. They illustrate how streamers employ various exploitative monetization marketing techniques, namely integrating elements based on chance: unpredictable rewards, in-game predictions, and giveaways. These techniques are considered OG marketing yet are still within the lines of Twitch gambling legislation causing concern for the safety of young naïve streamer enthusiasts. Lastly, Bouguettaya et al. (2020) consolidate that increases in gambling and problem gambling behaviours, as well as more favourable views towards gambling, are linked to exposure to gambling-related advertising. They assess research regarding gambling advertising with links to gambling attitudes, intentions, and behaviour. Together, I found that these studies underscore that the legality of OG marketing is highly superficial and unclear depending on the jurisdiction and type of advertising. Additionally, they all agree that more high-quality quantitative, experimental, and long-term research should be conducted on the effects of OG marketing, and governments should be held accountable for enforcing policies detailing the legal distribution and restrictions of OG marketing amongst youth to ensure safety.',
+		references: [],
+		download: {
+			url: '/written-responses/G1_Joshua_23020050_Written-Response_vFinal.pdf',
+			filename: 'G1_Joshua_23020050_Written-Response_vFinal.pdf'
+		},
+		grade: {
+			received: 40,
+			maximum: 40
+		}
+	},
+	{
+		query: 'shaan',
+		authors: [
+			{
+				firstName: 'Shaan',
+				lastName: 'Mohammad Ibne Javed Soyfoo',
+				studentId: '23030935'
+			}
+		],
+		title:
+			'A brief written response on negative financial consequences of online gambling amongst the youth',
+		content:
+			"Online gambling (OG) has become increasingly prevalent recently, and while it offers entertainment and the potential for monetary gain, it also has detrimental financial effects. However, the main concerns are the negative consequences, and this brief written response will explore some of the key’s financial pitfalls amongst youngsters. Firstly, Sarfaraz Khan (2022) highlights that the risk of money laundering is mostly high in OG due to the large amount of money present. Having said that, young individuals should not be related to OG at any cost, since this will certainly divert their minds from the right way of earning money, causing them to become involved in tax evasion and money laundering. Thomas et al. (2020) added that lost savings, debts, and bankruptcy are other financial harms, which prompts mostly online gamblers including the young ones, to keep on gambling hoping to recover their losses. His studies demonstrate that nowadays, young people are taking debt at unprecedented levels and the damage is irreversible since it affects their surroundings as they may be involved in more illegal activities such as fraud, theft, or embezzlement at an early age. Carolyn and Ryan (2010) support these arguments by saying that these individuals used various methods to hide their gambling problems by concealing evidence of their activities, for instance, betting receipt, statements, and card bills from others. Additionally, their research indicates that 61.3% of the gamblers questioned had argued about gambling and of those arguments 70.3% of them were related to money. When the youth engage in any kind gambling, they may use their family's resources or run up debts that affect the family's financial stability. Therefore, I can conclude that the negative consequences of online gambling amongst the youth are a growing concern since it may lead to their own substantial economic loss and strain relationships. Addressing this issue is necessary for the financial well-being of the young ones, family and society.",
+		references: [],
+		download: {
+			url: '/written-responses/G1_Shaan_23030935_Written-Response_vFinal.pdf',
+			filename: 'G1_Shaan_23030935_Written-Response_vFinal.pdf'
+		},
+		grade: {
+			received: 40,
+			maximum: 40
+		}
+	}
+];
