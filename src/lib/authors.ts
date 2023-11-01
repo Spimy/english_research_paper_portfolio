f03e8ea6535c3693155ac55ca00693c354ba870e
Spimy <officialspyme@gmail.com>
2023-11-01 17:03:33 +0800

    📦 NEW: Add centralised author array

---
+++ src/lib/authors.ts
@@ -0,0 +1,37 @@
+import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import type { Author } from './types/author';
+
+export const authors: Author[] = [
+	{
+		firstName: 'Joshua',
+		lastName: 'Edwin Rene Bonham',
+		studentId: '23020050',
+		course: 'Bachelor of Software Engineering (Hons)',
+		social: [{ icon: faInstagram, url: 'https://www.instagram.com/jb_bru' }],
+		title: 'Professional Gambler',
+		profileImage: 'https://placehold.co/400'
+	},
+	{
+		firstName: 'William',
+		lastName: 'Law Hong Waye',
+		studentId: '23020043',
+		course: 'Bachelor of Software Engineering (Hons)',
+		social: [
+			{ icon: faGlobe, url: 'https://www.spimy.dev' },
+			{ icon: faInstagram, url: 'https://www.instagram.com/william3001_lhw' },
+			{ icon: faGithub, url: 'https://www.github.com/spimy' }
+		],
+		title: 'Professional Gambler',
+		profileImage: 'https://placehold.co/400'
+	},
+	{
+		firstName: 'Shaan',
+		lastName: 'Mohammad Ibne Javed Soyfoo',
+		studentId: '23030935',
+		course: 'BSC (Hons) in Computer Science',
+		social: [{ icon: faInstagram, url: 'https://www.instagram.com/_shaan_s' }],
+		title: 'Professional Gambler',
+		profileImage: 'https://placehold.co/400'
+	}
+];
