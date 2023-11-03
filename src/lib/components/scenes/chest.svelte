ffa8f5fb70fa0818e8b3a338b00e579191a1a615
Spimy <officialspyme@gmail.com>
2023-11-04 01:32:54 +0800

    📦 NEW: Add animated chest for opening sequence

--- src/lib/components/scenes/chest.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { GLTF } from '@threlte/extras';
-
-	// const { gltf, actions } = useGltfAnimations<'Take 001'>();
-
-	// $: $actions['Take 001']?.play();
-</script>
-
-<GLTF url="/models/chest.glb" />
