53108040966ee735c77db4665b379d7f4ac7416d
Spimy <officialspyme@gmail.com>
2023-11-05 01:23:33 +0800

    📦 NEW: Add chips component

---
+++ src/lib/components/chips.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<div class="chips">
+	<img src="/roadmap/chip.png" alt="chip" class="chip" />
+	0
+</div>
+
+<style lang="scss">
+	.chips {
+		background-color: var(--clr-accent-100);
+		padding: 0.2rem 0.5rem;
+		border-radius: 1rem;
+		width: 5rem;
+
+		.chip {
+			aspect-ratio: 1;
+			width: 1rem;
+			display: inline-block;
+			margin: -0.1rem 0;
+		}
+	}
+</style>
