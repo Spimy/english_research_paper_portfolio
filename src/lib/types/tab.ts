8fe923b0b18efc105c81b48e2da8e611f80bf7ff
Spimy <officialspyme@gmail.com>
2023-11-14 21:29:22 +0800

    👌 IMPROVE: Add reference interface

--- src/lib/types/tab.ts
+++ src/lib/types/tab.ts
@@ -1,11 +1,19 @@
 import type { Author } from './author';
 
+export interface Reference {
+	author: string;
+	title: string;
+	publisher: string;
+	issuePage: string;
+	url: string;
+}
+
 export interface Tab {
 	query: string;
 	authors: Author[];
 	title: string;
 	content: string;
-	references: string[];
+	references: Reference[];
 	download: {
 		url: string;
 		filename: string;
