3a44611febad855dfb446400551b6483385dee67
Spimy <officialspyme@gmail.com>
2023-11-15 15:23:21 +0800

    📦 NEW: Add outline content

--- src/routes/outline/+page.svelte
+++ src/routes/outline/+page.svelte
@@ -34,6 +34,8 @@
 		</div>
 	</header>
 
+	<hr />
+
 	<article class="outline__body">
 		<div class="outline__body__intro">
 			<h2>Introduction</h2>
@@ -47,8 +49,9 @@
 			<div class="outline__body__intro__general">
 				<h3>General Information</h3>
 				<p>
-					&lt;Gambling history&gt;. With the rise of IT and internet capabilities, online gambling
-					has become increasingly available for anyone with internet access around the world.
+					<i>&lt;Gambling history&gt;</i>. With the rise of IT and internet capabilities, online
+					gambling has become increasingly available for anyone with internet access around the
+					world.
 				</p>
 			</div>
 			<div class="outline__body__intro__specific">
@@ -73,6 +76,171 @@
 				</ol>
 			</div>
 		</div>
+
+		<hr />
+
+		<div class="outline__body__mp--1">
+			<h2>Main Point 1: Mental health</h2>
+			<div>
+				<h3>1st Supporting Information - Addiction</h3>
+				<p>Potential contribution to excessive gambling behaviors:</p>
+				<ul>
+					<li>Large wagers</li>
+					<li>Continuous gambling</li>
+					<li>Rapid feedback</li>
+					<li>Easy access</li>
+				</ul>
+				<p>
+					Internet use, including gaming, has led to calls for further research and specific
+					consideration for pathological Internet gambling due to similarities with other
+					addictions. (Gainsbury, 2023)
+				</p>
+			</div>
+			<div>
+				<h3>2nd Supporting Information - Problem Gambling and Psychological Distress</h3>
+				<p>
+					depression, social isolation, strained relationships, diminished self-esteem (Livazović,
+					2019). Potential cause: FOMO (N.Lakić et al., 2023)
+				</p>
+			</div>
+			<div>
+				<h3>3rd Supporting Information - Cognitive Bias</h3>
+				<p>Sunk Cost Fallacy (Tait, 2019)</p>
+			</div>
+		</div>
+
+		<hr />
+
+		<div class="outline__body__mp--2">
+			<h2>Main Point 2: Negative Financial Consequences of Online Gambling</h2>
+			<div>
+				<h3>1st Supporting Information</h3>
+				<p>Online gambling and money laundering (Sarfaraz Khan, 2022)</p>
+			</div>
+			<div>
+				<h3>2nd Supporting Information</h3>
+				<p>Gambling related consumer credit use and debt problems (Thomas & Sally, 2019)</p>
+			</div>
+			<div>
+				<h3>3rd Supporting Information</h3>
+				<p>Gambling and debt: the hidden impacts on family and work life (Carolyn & Ryan, 2010)</p>
+			</div>
+		</div>
+
+		<hr />
+
+		<div class="outline__body__mp--3">
+			<h2>Main Point 3: Consumer Subjectivity</h2>
+			<div>
+				<h3>1st Supporting Information - Consumer Subjectivity</h3>
+				<p>(Potolia, 2021) or (López-González, 2017)</p>
+				<ul>
+					<li>Skill-enhancing advertising</li>
+					<li>Risk-lowering advertising</li>
+					<li>Enticing colors, designs, wordplay</li>
+					<li>Betting connected to 'harmless' activities (i.e., gaming, sports)</li>
+				</ul>
+			</div>
+			<div>
+				<h3>2nd Supporting Information - Online Influencers Prey on Vulnerable Teenagers</h3>
+				<p>Twitch (Abarbanel & Johnson, 2020)</p>
+				<ul>
+					<li>Use online 'community', raffles, Bits.</li>
+					<li>Twitch currency</li>
+					<li>Exploitation of viewers</li>
+				</ul>
+			</div>
+			<div>
+				<h3>3rd Supporting Information - Dose-Response</h3>
+				<p>
+					'Dose-response' relationship between gambling advertising and gambling attitudes
+					(Bouguettaya et al.,2020)
+
+					<br />
+
+					Tie in: the risk of fraud and naïve youthful users - popular YouTuber accounts hacked,
+					fake cryptos were “sold” using Elon Musk's image.
+				</p>
+			</div>
+		</div>
+
+		<hr />
+
+		<div class="outline__body__mp--4">
+			<h2>Main Point 4 (Counterargument): Learning experiences</h2>
+			<div>
+				<h3>1st Supporting Information</h3>
+				<p>
+					In the context of online gambling, such as gacha games, there is KeqingMains website to
+					prove that there are players who take the game seriously and learned to reverse engineer
+					the game to maximize gains
+					<br />
+					What can be learned about gambling from a learning perspective? (Ramnero, 2019)
+				</p>
+			</div>
+			<div>
+				<h3>2nd Supporting Information</h3>
+				<p>
+					There are other, safer, alternatives to learning the same skills such as formal education
+					like universities.
+				</p>
+				<ul>
+					<li>Knowledge and skills</li>
+					<li>Financial stability</li>
+					<li>Career prospects</li>
+					<li>Reduced risk of addiction</li>
+					<li>Social and family relationships</li>
+					<li>Legal and ethical consideration</li>
+					<li>Emotional and mental well-being</li>
+				</ul>
+				<p>(Scott et al., 2017)</p>
+			</div>
+			<div>
+				<h3>3rd Supporting Information (Refutation)</h3>
+				<p>
+					Not all gambles can be predicted like horse betting. The risk is too high for practical
+					loss.
+					<br />
+					Gamblers tend to lose a lot of money making them realize that gambling is not profitable until
+					the urge of betting kicks them and they’re up to throwing money around again.
+					<br />
+					Winning and losing in online gambling: effects on within-session chasing (Chen et al., 2022)
+				</p>
+			</div>
+		</div>
+
+		<hr />
+
+		<div class="outline__body__conclusion">
+			<h2>Conclusion</h2>
+			<div>
+				<h3>Summary of Main Points</h3>
+				<p>
+					Mental illnesses (psychological distress), unregulated deceptive marketing, and negative
+					financial consequences.
+				</p>
+			</div>
+			<div>
+				<h3>Restate Stand and Thesis</h3>
+				<p>
+					[Online gambling should indubitably be sanctioned by international government to regulate
+					usage among youth, improve online safety and decrease widespread fraud.]
+				</p>
+			</div>
+		</div>
+		<div>
+			<h3>Final Statement</h3>
+			<p>Government should sanction online gambling amongst youth.</p>
+			<ul>
+				<li>
+					Form strict and detailed policies on online gambling websites i.e., implement age
+					detection outlines – users should provide ID before logging into OG sites. (Long login
+					process and less anonymity may discourage young adults)
+				</li>
+				<li>Create an OG crime unit to enforce legal action on fraudulent OG websites.</li>
+				<li>Promote anti-gambling campaigns amongst youth</li>
+			</ul>
+		</div>
 	</article>
 </section>
 
@@ -86,9 +254,24 @@
 			font-size: var(--fs-600);
 		}
 
-		p {
+		h2 {
+			font-size: var(--fs-500);
+		}
+
+		h3 {
+			font-size: var(--fs-400);
+		}
+
+		p,
+		li {
 			line-height: 2em;
 			font-size: 1.25rem;
+			max-width: 80ch;
+			text-align: justify;
+		}
+
+		hr {
+			width: 100%;
 		}
 
 		&__header {
@@ -103,6 +286,14 @@
 
 			a {
 				color: var(--clr-accent-100);
+			}
+		}
+
+		&__body {
+			div {
+				&:not(:first-of-type) {
+					margin-block-start: 2rem;
+				}
 			}
 		}
 	}
