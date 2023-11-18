84bf4362d4a87864eb7a5eb3b92e91f121ac5329
Spimy <officialspyme@gmail.com>
2023-11-18 14:34:24 +0800

    📦 NEW: Add content of first draft

---
+++ src/lib/tabs/drafts.ts
@@ -0,0 +1,63 @@
+import { authors } from '$lib/authors';
+import type { Tab } from '$lib/types/tab';
+
+export const drafts: Tab[] = [
+	{
+		query: 'draft-v1',
+		authors: [...authors],
+		title: 'First Draft',
+		content: [
+			{
+				title: '1.0 Introduction',
+				body: [
+					'The pandemic of online gambling (OG) is destroying the lives of today’s youth at a drastically increasing rate (Montial et al., 2021). Gambling, often termed betting, is the wagering of anything with value for the chance of achieving a certain event or outcome. It has existed in almost every culture since the beginning of civilisation. For instance, at the base of His crucifixion, Jesus’ garments became a wager for Roman soldiers (Eadington, 2008). Subsequently, it has evolved considerably; however, its legality has been questioned numerous times (as seen in European history) (Gambling: History, 2023).',
+					'Additionally, with the mass advancement in IT and internet capabilities, OG has become increasingly accessible and immediate for anyone with internet worldwide. Whether betting on sporting events or predicting political elections, nowadays there is a lot of money to be lost and won from the comfort of one’s home. With an estimated market value of $63.53 billion (in 2022) and a compound annual growth rate of 11.7% (Research and Markets, n.d.), OG will only get bigger with the increase in betting websites, fake ‘get rich quick’ propaganda, and controversial international legality.',
+					'This research paper contends that governments worldwide should indubitably sanction OG to regulate usage among younger generations, improve online safety, and decrease widespread fraud. It specifically looks at the moral problem of mental health issues brought on by OG, the financial difficulties posed by widespread use, and the moderately unregulated deceptive marketing of OG and its ramifications amongst youth.'
+				]
+			},
+			{
+				title: '2.0 Youth online gambling and the development of mental health issues',
+				body: [
+					'Gambling leading to various mental health issues is nothing new, but one can control who is allowed to engage in traditional forms of gambling. With the rise of the internet and OG, however, there are very little that has been done to prevent who participates in them, making them very easily accessible (Gainsbury, 2015). In fact, they are so much easier to access that the earliest recorded age who participated in gambling was 7 and 5.8 percent of adolescents have admitted to OG (Livazović & Bojčić, 2019). This is very concerning as someone at that age should instead be participating in activities that support their physical, emotional, and cognitive development, not damage them.',
+					'One of the easiest ways for someone so young to get in contact with OG is through gaming, specifically mobile gaming. This is due to the rise of Gacha games which have been labelled as a form of gambling due to shared, similar, mechanics (Lakić et al., 2023). The problem is that many players who engage in OG do not have any source of income and rely solely on their parents (Lakić et al., 2023). This can create distrust among the family and develop other forms of mental illnesses such as depression, anxiety, substance abuse, low self-esteem, and delinquency (Livazović & Bojčić, 2019. As it currently stands, there are no ways to prevent young children from accessing these games, not even age restrictions as those are only valid for the content of a video game (Games Ratings Authority, n.d.) and do not consider microtransactions.',
+					'Moreover, cognitive biases are also developed. The most notable one is the Sunk Cost Fallacy (SCF). Tait et al. (2019) explained that it is a cognitive bias where the tendency for someone to make irrational decisions base on past investments, whether is in the form of time or money. They also highlighted that adolescents are the primary victim when money is involved which is quite worrying. Adults in general are less prone to SCF due to age-related differences in information processing and are less likely to focus exclusively on losses (Strough et al., 2008) but if SCF is developed from a young age, it is possible that the bias is carried over to adulthood.'
+				]
+			},
+			{
+				title: '3.0 Youth online gambling and financial consequences',
+				body: [
+					"The surge in online gambling (OG) among today's youth has become a significant social concern. Armitage (2021) mentioned that the widespread of digital platforms provides unlimited access to young individuals which lured them into the captivating world of online betting and blinds them to the negative long and short-term consequences on their financial well-being as well as their surroundings.",
+					'Importantly, according to Sarfaraz Khan (2022), the probability of money laundering is particularly high due to the substantial amounts of money involved. Young individuals must avoid any association with OG, as such involvement could lead them away from legitimate ways of earning money, engaging them in money laundering and tax evasion.',
+					'Additionally, Swanton and Gainsbury (2020) underscored that financial consequences such as loss savings, accruing debts, and potential bankruptcy are additional adversities affecting primarily online gamblers, including the younger ones. This financial strain compels many to persist in gambling hoping for a big win. Their research further highlights a contemporary trend where young individuals are increasingly accumulating debt at unprecedented rates, leading to irreversible loss. This not only impacts their personal financial well-being but also their surroundings, potentially involving them in illicit activities like fraud, theft, or embezzlement from an early age.',
+					"Downs and Woolrych (2010) also reinforce these assertions by saying that individuals with gambling issues employ diverse strategies to mask their problems, concealing evidence. Furthermore, this study reveals that 61.3% of surveyed gamblers had engaged in arguments related to gambling, with 70.3% of these disputes revolving around finance. When young individuals partake in any form of gambling, they may leverage their family's resources or accumulate debts that jeopardize the overall financial stability of the family.",
+					'Moreover, Oksanen et al. (2019) emphasize on the fact that OG poses a potential risk, especially for the young. The emergence of new OG forms introduces potential risks to the younger generation and it is necessary for professionals to recognize the influence of OG and its associated activities. Consequently, new policies should certainly be developed to address these challenges fostering the youth a more secure financial future.'
+				]
+			},
+			{
+				title: '4.0 Ramifications of the moderately unregulated deceptive marketing of OG',
+				body: [
+					"The unregulated deceptive marketing of OG presents a grave concern, with multifaceted issues that extend far beyond the mere pursuit of financial gain. Firstly, the augmentation of advertising and marketing for OG deliberately taps into young consumer subjectivity. Deceptive marketing strategies capitalise on individuals' cognitive biases and intrinsic senses. Enticing colors, designs, and wordplay are used to lure potential gamblers with promises of excitement and big wins. This preys on the natural inclination of the human’s heuristic of representativeness (López-González et al., 2017). Individuals feel optimistic about their chances, drawing them into a dangerous web of gambling addiction and financial losses.",
+					'Many OG advertisements portray gambling as a safe, skill-enhancing, low-risk activity and associate betting with seemingly harmless activities, such as socializing and entertainment. According to research López-González et al. (2017) gathered, supporters of the Australian Rugby League, many of whom are families with pubescent children, were subject to 322 episodes of gambling advertisements throughout three matches (López-González et al., 2017). They argue, as a result, that minors may be susceptible to early-onset gambling addiction. Thus, younger generations are systematically desensitized to perceive OG as an investment or a hobby. However, the reality is that OG can have severe financial and psychological consequences for vulnerable individuals.',
+					'Furthermore, Bouguettaya et al. (2020) consolidate this dose-response relationship between gambling advertising and gambling attitudes. The more exposure individuals must have to gambling advertisements, the more individuals are prone to developing positive attitudes toward gambling (Bouguettaya et al., 2020). This ultimately contributes to an increase in problem gambling cases among youth.',
+					'Moreover, streamers use online streaming platforms, such as Twitch, to employ various exploitative monetization OG marketing techniques, namely integrating elements based on chance: unpredictable rewards, in-game predictions, and giveaways (Abarbanel et al., 2020). These influencers, along with celebrity-endorsed advertising, often glamorize and romanticize the world of OG, showcasing luxurious lifestyles and big winnings. Adolescents, in their formative years, are particularly susceptible to these tactics as they see these influencers as their role models. This type of OG marketing is still within the lines of Twitch gambling legislation, causing concern for the safety of young, naïve streamer enthusiasts.',
+					'The legality of OG marketing is highly superficial and unclear, depending on the jurisdiction and type of advertising. Governments and regulatory bodies should be held accountable for enforcing policies detailing the legal distribution and restrictions of OG marketing amongst youth to ensure safety. More high-quality quantitative, experimental, and long-term research should be conducted on the effects of OG and the insidious nature of unrestricted marketing. Younger generations are suffering terrible repercussions from a lack of intervention, which is causing a rise in addiction rates and socioeconomic costs.'
+				]
+			},
+			{
+				title: '5.0 Counterargument',
+				body: ['WIP']
+			},
+			{
+				title: '6.0 Conclusion',
+				body: [
+					'Consequently, more efforts are required to address these concerns and develop responsible gambling practices as beyond psychological consequences, they also raise ethical and legal questions. The exploitability of these behaviours, particularly among younger individuals, demands for stricter legal frameworks that are to be obeyed so as the vulnerable young ones are protected. Stricter obedience to legal frameworks and a deeper understanding of SCF, its effects on mental health, the negative impact on one’s financial situation, and how it ties into the addictive gaming mechanics like Gacha games are essential to promote responsible OG and mitigate potential harm. One way to prevent youth OG, and discouraging it all together in general, is the (enforced) requirement of verifying one’s identity during the registration stage. In essence, when an individual registers an account on an OG website, they must upload an image of themself holding their ID card. This extra step makes sure the individual is at least of age. Moreover, users are generally put off by multi-step registrations on any website, and with the need of an identity verification, it only diminishes their will to continue as their anonymity is further reduced. To ensure that users are well-informed and protected while engaging in OG, both legal measures and educational initiatives can curb the psychological impacts induced by it.'
+				]
+			}
+		],
+		references: [],
+		download: {
+			url: '',
+			filename: ''
+		}
+	}
+];
