d49690fc34d219a9cb9b9d4a7dea831bc848703c
Spimy <officialspyme@gmail.com>
2023-11-20 00:31:29 +0800

    👌 IMPROVE: Update style of modal

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -18,12 +18,17 @@
 {#if typeof bgText !== 'undefined' || !closed}
 	<div
 		class="modal-container"
-		out:fly={{ x: 2000, duration: enableSlide ? 1000 : 0, easing: quintOut }}
+		out:fly={{ x: 5000, duration: enableSlide ? 5000 : 0, easing: quintOut, opacity: 1 }}
 	>
 		{#if closed}
 			<div class="modal-container__closed-content">
 				<h1>{bgText}</h1>
-				<button on:click={() => (closed = false)}>Reopen</button>
+				<button
+					class="modal-container__closed-content__reopen-btn btn btn--invert"
+					on:click={() => (closed = false)}
+				>
+					Reopen
+				</button>
 			</div>
 		{:else}
 			<div class="modal-container__modal">
@@ -39,23 +44,38 @@
 		position: fixed;
 		z-index: 9999;
 		inset: 0;
-		background-color: red;
+		background-color: var(--clr-cta-100);
+		background-image: url('/curtain.jpg');
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
 		display: grid;
 		place-items: center;
 		padding: 1rem var(--content-padding);
 
-		&__modal {
-			background-color: blue;
+		&__modal,
+		&__closed-content {
+			background-color: rgba(0, 0, 0, 0.75);
+			border: 0.2rem solid var(--clr-cta-100);
 			padding: 1rem;
 			border-radius: 0.5rem;
 			display: grid;
 
-			&__close-btn {
+			button {
 				cursor: pointer;
-				background-color: transparent;
-				border: none;
-				justify-self: end;
 			}
+		}
+
+		&__modal__close-btn {
+			color: var(--clr-neutral-100);
+			background-color: transparent;
+			font-weight: 600;
+			border: none;
+			justify-self: end;
+		}
+
+		&__closed-content__reopen-btn {
+			margin-block-start: 1rem;
 		}
 	}
 </style>
