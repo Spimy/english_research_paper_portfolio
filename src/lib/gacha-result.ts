5f186cd2e70320038d0b57d2cef0b49606b3997e
Spimy <officialspyme@gmail.com>
2023-11-26 17:32:01 +0800

    👌 IMPROVE: Update roadmap gacha result

--- src/lib/gacha-result.ts
+++ src/lib/gacha-result.ts
@@ -6,11 +6,15 @@ export const gachaResult: GachaResult[] = [
 		rarity: 'R'
 	},
 	{
-		url: '/roadmap/gacha/Week_07.png',
-		rarity: 'SR'
+		url: '/roadmap/gacha/Week_03.png',
+		rarity: 'R'
 	},
 	{
-		url: '/roadmap/gacha/Week_01-02.png',
+		url: '/roadmap/gacha/Week_04.png',
+		rarity: 'R'
+	},
+	{
+		url: '/roadmap/gacha/Week_05-06.png',
 		rarity: 'R'
 	},
 	{
@@ -18,11 +22,11 @@ export const gachaResult: GachaResult[] = [
 		rarity: 'SR'
 	},
 	{
-		url: '/roadmap/gacha/Week_01-02.png',
+		url: '/roadmap/gacha/Week_08.png',
 		rarity: 'R'
 	},
 	{
-		url: '/roadmap/gacha/Week_07.png',
+		url: '/roadmap/gacha/Week_09.png',
 		rarity: 'SR'
 	},
 	{
@@ -30,15 +34,11 @@ export const gachaResult: GachaResult[] = [
 		rarity: 'SSR'
 	},
 	{
-		url: '/roadmap/gacha/Week_07.png',
-		rarity: 'SR'
-	},
-	{
-		url: '/roadmap/gacha/Week_01-02.png',
+		url: '/roadmap/gacha/Week_12.png',
 		rarity: 'R'
 	},
 	{
-		url: '/roadmap/gacha/Week_07.png',
+		url: '/roadmap/gacha/Week_13.png',
 		rarity: 'SR'
 	}
 ];
