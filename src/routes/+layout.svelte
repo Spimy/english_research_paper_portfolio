889a68a3eb05c7d61a0e50b89a422025f0494316
Spimy <officialspyme@gmail.com>
2023-12-10 00:28:32 +0800

    👌 IMPROVE: Preload sound effects on load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,22 @@
 	import Footer from '$lib/components/footer.svelte';
 	// import Modal from '$lib/components/modal.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
+	import { onMount } from 'svelte';
 	// import SlotMachine from '$lib/components/slot-machine.svelte';
 	import '../scss/main.scss';
 	import type { LayoutServerData } from './$types';
 
 	export let data: LayoutServerData;
+
+	onMount(() => {
+		// Preload roadmap sound effects
+		fetch('/sfx/WishMenu.mp3');
+		fetch('/sfx/Opening.mp3');
+		fetch('/sfx/R.mp3');
+		fetch('/sfx/SR.mp3');
+		fetch('/sfx/SSR.mp3');
+		fetch('/sfx/Summary.mp3');
+	});
 </script>
 
 <!-- <Modal
